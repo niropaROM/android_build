bfd7ae86951a1e41a17a997e3e187cefd44508f7
George G <kreach3r@users.noreply.github.com>
2015-10-09 11:08:08 +0300

    Disable the after flash image verification
    This verification produced failed installations on MultiROM. Get rid of it until further notice.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1220,7 +1220,6 @@ class BlockDifference(object):
     if progress:
       script.ShowProgress(progress, 0)
     self._WriteUpdate(script, output_zip)
-    self._WritePostInstallVerifyScript(script)
 
   def WriteVerifyScript(self, script):
     partition = self.partition
